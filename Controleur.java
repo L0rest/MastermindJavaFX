4fbc42ef66fd8b148e178f8b1528f81903dc0872
L0rest <jawad.maache@gmail.com>
2023-01-13 18:20:43 +0100

    Set initialization + method test

--- Controleur.java
+++ Controleur.java
@@ -1,61 +1,19 @@
 package ProjetJava.MastermindJavaFX;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.Random;
 
-public class Controleur implements ActionListener, MouseListener {
+import javafx.fxml.Initializable;
 
-    Modele modl;
-    Random rand;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public Controleur(Modele m) {
-        this.modl = m;
-        this.rand = new Random();
-        for (int i = 0; i < Modele.DIFFICULTE; i++) {
-            this.modl.combinaison.jetons[i] = Modele.COULEURS[rand.nextInt(Modele.COULEURS.length)];
-        }
-        this.modl.propositions[0] = new Rangee();
+public class Controleur implements Initializable {
+
+    public void test(javafx.event.ActionEvent actionEvent) {
+        System.out.println("test");
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        Color couleur = new Color(Integer.parseInt(e.getActionCommand()));
-        if (this.modl.état == Modele.Etat.EN_COURS) {
-            this.modl.completer_prop(couleur);
-        }
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getButton() == MouseEvent.BUTTON1) {
-            if (e.getClickCount() == 2) {
-                this.modl.annuler();
-            }
-        }
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
