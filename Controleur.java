b5181471a71848cc86296df6c5c3d8e42b53091d
L0rest <jawad.maache@gmail.com>
2023-01-18 16:22:23 +0100

    Method drawShapes added

--- Controleur.java
+++ Controleur.java
@@ -2,6 +2,8 @@ package ProjetJava.MastermindJavaFX;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 
 import java.awt.*;
@@ -11,6 +13,7 @@ import java.util.ResourceBundle;
 
 public class Controleur implements Initializable {
 
+    public Canvas canvas;
     Modele modl;
     public HashMap<String,Color> couleurs = new HashMap<>();
 
@@ -29,13 +32,43 @@ public class Controleur implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         this.modl = new Modele();
         insertionsCouleursDico();
+        drawShapes();
     }
 
     public void addColor(ActionEvent actionEvent) {
         String s = (((Button) actionEvent.getSource()).getBackground().getFills().get(0).getFill()).toString();
-
         this.modl.completer_prop(couleurs.get(s));
 
+        drawShapes();
+    }
+
+    public void drawShapes() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+
+
+        for (int i = 0; i < this.modl.propositions.length; i++) {
+            if (this.modl.propositions[i] != null) {
+                for (int j = 0; j < ProjetJava.MastermindJavaFX.Modele.DIFFICULTE; j++) {
+                    if (this.modl.propositions[i].jetons[j] != null) {
+                        Color currColor = this.modl.propositions[i].jetons[j];
+                        javafx.scene.paint.Paint fxColor = javafx.scene.paint.Color.rgb(currColor.getRed(), currColor.getGreen(), currColor.getBlue(), 1.0);
+                        gc.setFill(fxColor);
+                        gc.fillOval((60 * j) + 30, (60 * i) + 10, 40, 40);
+                    }
+                }
+                for (int j = 0; j < this.modl.propositions[i].noirs + this.modl.propositions[i].blancs; j++) {
+                    if (j < this.modl.propositions[i].noirs) {
+                        gc.setFill(javafx.scene.paint.Color.BLACK);
+                        gc.fillOval(270 + (20 * j), (60 * i) + 10, 15, 15);
+                    } else {
+                        gc.setFill(javafx.scene.paint.Color.WHITE);
+                        gc.fillOval(270 + (20 * j), (60 * i) + 10, 15, 15);
+                    }
+                }
+
+            }
+        }
     }
 
 }
